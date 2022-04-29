88508cfa79d7636b2369d8fd7aa65fc792610c0a
xiang ni <xiang_x_ni@manulife.com>
2022-04-29 17:56:02 +0800

    update

--- server-slide-rendering/providers/app-context/app-context.service.ts
+++ server-slide-rendering/providers/app-context/app-context.service.ts
@@ -1,9 +1,7 @@
 import { InjectorToken } from '@di';
 import { AppContextService as SharedAppContextService, Fetch } from '@shared/providers/app-context';
-import { Observable } from 'rxjs';
 
 export const FETCH = InjectorToken.get('FETCH');
-export type readFileType = (url: string) => Observable<object>;
 
 export class AppContextService extends SharedAppContextService {
   get fetch(): Fetch {
