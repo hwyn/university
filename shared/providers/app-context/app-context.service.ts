a3649ef1528162e61fd69486497f7bfab9cded2d
xiang ni <xiang_x_ni@manulife.com>
2022-05-09 17:35:36 +0800

    update share

--- shared/providers/app-context/app-context.service.ts
+++ shared/providers/app-context/app-context.service.ts
@@ -1,6 +1,6 @@
 import { Inject, InjectorToken, LocatorStorage } from '@di';
-import { MicroManageInterface } from '@shared/micro/types';
-import { ENVIRONMENT } from '@shared/token';
+import { MicroManageInterface } from '../../micro/types';
+import { ENVIRONMENT } from '../../token';
 import { cloneDeep } from 'lodash';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
